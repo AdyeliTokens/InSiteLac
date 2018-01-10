fd8c88a6ed49f7736df6440f50b9337c4800d1e7
Enrique Chan Fernandez <enrique.nec@gmail.com>
2018-01-10 17:25:23 -0600

    Centrado de columnas

--- Assets/Vuforia/Scripts/ImportSpheres.cs
+++ Assets/Vuforia/Scripts/ImportSpheres.cs
@@ -12,7 +12,7 @@ public class ImportSpheres : MonoBehaviour
         // Pull down the JSON from our web-service
         WWW w = new WWW("https://serverpmi.tr3sco.net/api/KPIs");
         yield return w;
-        
+
         print("Waiting for sphere definitions\n");
 
         // Add a wait to make sure we have the definitions
@@ -41,20 +41,23 @@ public class ImportSpheres : MonoBehaviour
         string json2 = "{\"valores\":" + json + "}";
         var items = KPICollection.CreateFromJSON(json2);
         print("Conversion");
-        float x = -0.9f, y = 0, z = 2, r = 0.03f;
+        float x = 0, y = -0.5f, z = 3, r = 0.03f;
+        x = -(0.06f * ((items.valores.Count / 2)));
+        int columna = 0;
         foreach (var item in items.valores)
         {
-
+            columna++;
             // Gather center coordinates, radius and level
-            
+
             int level = 1;
 
-            
 
-            Vector3 v = new Vector3(x , y, z);
 
-            
+            Vector3 v = new Vector3(x, y, z);
+
+
             var sphere = GameObject.CreatePrimitive(PrimitiveType.Cube);
+
             float float_YTD = Convert.ToSingle(item.YTD);
             sphere.transform.position = new Vector3(x, y + (float_YTD / 2), z);
             x = x + (2 * (r + 0.002f));
@@ -63,55 +66,33 @@ public class ImportSpheres : MonoBehaviour
             sphere.transform.localScale = new Vector3(d, d + float_YTD, d);
 
             UnityEngine.Color col = UnityEngine.Color.white;
-            switch (level)
+            switch (columna)
             {
-
                 case 1:
-
                     col = UnityEngine.Color.red;
-
                     break;
-
                 case 2:
-
                     col = UnityEngine.Color.yellow;
-
                     break;
-
                 case 3:
-
-                    col = UnityEngine.Color.green;
-
+                   col = UnityEngine.Color.green;
                     break;
-
                 case 4:
-
                     col = UnityEngine.Color.cyan;
-
                     break;
-
                 case 5:
-
                     col = UnityEngine.Color.magenta;
-
                     break;
-
                 case 6:
-
                     col = UnityEngine.Color.blue;
-
                     break;
-
                 case 7:
-
                     col = UnityEngine.Color.grey;
-
+                    columna = 0;
                     break;
-
             }
 
             sphere.GetComponent<Renderer>().material.color = col;
-
 
 
         }
