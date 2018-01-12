62c996cebc56b3182fb5d37376bed4748c85075b
Enrique Chan Fernandez <enrique.nec@gmail.com>
2018-01-12 11:26:23 -0600

    Cambio para posicionar la hoja

--- Assets/Vuforia/Scripts/ImportSpheres.cs
+++ Assets/Vuforia/Scripts/ImportSpheres.cs
@@ -3,9 +3,11 @@ using System.Collections;
 using System.Collections.Generic;
 using System;
 
-public  class ImportSpheres : MonoBehaviour
+public class ImportSpheres : MonoBehaviour
 {
     private List<GameObject> sphereList;
+    public GameObject content;
+    
 
     public IEnumerator DownloadSpheres()
     {
@@ -44,64 +46,114 @@ public  class ImportSpheres : MonoBehaviour
     {
         string json2 = "{\"valores\":" + json + "}";
         var items = KPICollection.CreateFromJSON(json2);
-        print("Conversion");
-        float x = 0, y = -0.5f, z = 3, r = 0.03f;
-        x = -(0.06f * ((items.valores.Count / 2)));
+
+        //float x = 0, y = -0.01f, z = 0, r = 0.03f;
+        float x = 0.79f, y = 0.05f, z = -1.476f, r = 0.03f;
+        //x = -(0.06f * ((items.valores.Count / 2)));
         int columna = 0;
-        foreach (var item in items.valores)
+
+        Vector3 v = new Vector3(x, y, z);
+
+        GameObject sphere = GameObject.CreatePrimitive(PrimitiveType.Cube);
+        
+        sphere.transform.SetParent(content.gameObject.transform, false);
+        //float float_YTD = Convert.ToSingle(item.YTD);
+        //sphere.transform.position = new Vector3(x, y + (float_YTD / 2), z);
+        sphere.transform.position = content.gameObject.transform.position;
+
+
+
+        //x = x + (2 * (r + 0.002f));
+        float d = 2 * r;
+
+        //sphere.transform.localScale = new Vector3(d, d + float_YTD, d);
+        sphere.transform.localScale = new Vector3(d, d, d);
+
+        UnityEngine.Color col = UnityEngine.Color.white;
+        switch (columna)
         {
-            columna++;
-            // Gather center coordinates, radius and level
-
-            int level = 1;
-
-
-
-            Vector3 v = new Vector3(x, y, z);
-
-
-            GameObject sphere = GameObject.CreatePrimitive(PrimitiveType.Cube);
-
-            float float_YTD = Convert.ToSingle(item.YTD);
-            sphere.transform.position = new Vector3(x, y + (float_YTD / 2), z);
-            x = x + (2 * (r + 0.002f));
-            float d = 2 * r;
-
-            sphere.transform.localScale = new Vector3(d, d + float_YTD, d);
-
-            UnityEngine.Color col = UnityEngine.Color.white;
-            switch (columna)
-            {
-                case 1:
-                    col = UnityEngine.Color.red;
-                    break;
-                case 2:
-                    col = UnityEngine.Color.yellow;
-                    break;
-                case 3:
-                    col = UnityEngine.Color.green;
-                    break;
-                case 4:
-                    col = UnityEngine.Color.cyan;
-                    break;
-                case 5:
-                    col = UnityEngine.Color.magenta;
-                    break;
-                case 6:
-                    col = UnityEngine.Color.blue;
-                    break;
-                case 7:
-                    col = UnityEngine.Color.grey;
-                    columna = 0;
-                    break;
-            }
-
-            sphere.GetComponent<Renderer>().material.color = col;
-
-            sphereList.Add(sphere);
+            case 1:
+                col = UnityEngine.Color.red;
+                break;
+            case 2:
+                col = UnityEngine.Color.yellow;
+                break;
+            case 3:
+                col = UnityEngine.Color.green;
+                break;
+            case 4:
+                col = UnityEngine.Color.cyan;
+                break;
+            case 5:
+                col = UnityEngine.Color.magenta;
+                break;
+            case 6:
+                col = UnityEngine.Color.blue;
+                break;
+            case 7:
+                col = UnityEngine.Color.grey;
+                columna = 0;
+                break;
         }
 
-        
+        sphere.GetComponent<Renderer>().material.color = col;
+
+
+
+        //foreach (var item in items.valores)
+        //{
+        //    columna++;
+        //    Vector3 v = new Vector3(x, y, z);
+
+        //    GameObject sphere = GameObject.CreatePrimitive(PrimitiveType.Cube);
+        //    sphere.transform.parent = content.gameObject.transform;
+        //    //float float_YTD = Convert.ToSingle(item.YTD);
+        //    //sphere.transform.position = new Vector3(x, y + (float_YTD / 2), z);
+        //    sphere.transform.position = new Vector3(x, y , z);
+
+
+
+        //    //x = x + (2 * (r + 0.002f));
+        //    float d = 2 * r;
+
+        //    //sphere.transform.localScale = new Vector3(d, d + float_YTD, d);
+        //    sphere.transform.localScale = new Vector3(d, d , d);
+
+        //    UnityEngine.Color col = UnityEngine.Color.white;
+        //    switch (columna)
+        //    {
+        //        case 1:
+        //            col = UnityEngine.Color.red;
+        //            break;
+        //        case 2:
+        //            col = UnityEngine.Color.yellow;
+        //            break;
+        //        case 3:
+        //            col = UnityEngine.Color.green;
+        //            break;
+        //        case 4:
+        //            col = UnityEngine.Color.cyan;
+        //            break;
+        //        case 5:
+        //            col = UnityEngine.Color.magenta;
+        //            break;
+        //        case 6:
+        //            col = UnityEngine.Color.blue;
+        //            break;
+        //        case 7:
+        //            col = UnityEngine.Color.grey;
+        //            columna = 0;
+        //            break;
+        //    }
+
+        //    sphere.GetComponent<Renderer>().material.color = col;
+
+
+
+        //    sphereList.Add(sphere);
+        //}
+
+
 
     }
 
@@ -111,7 +163,7 @@ public  class ImportSpheres : MonoBehaviour
         {
             Destroy(item);
         }
-        
+
     }
 
     void Update()
