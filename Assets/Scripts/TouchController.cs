6d8411c42b185620756d0a024c006834c6a7e549
Enrique Chan Fernandez <enrique.nec@gmail.com>
2018-01-14 20:14:20 -0600

    Ponerle titulo a las graficas

--- Assets/Scripts/TouchController.cs
+++ Assets/Scripts/TouchController.cs
@@ -40,7 +40,7 @@ public class TouchController : MonoBehaviour
                     string nombre = hit.transform.gameObject.name;
                     if (nombre != "arbol")
                     {
-                        message.text= "Importando informacion de " + nombre + "...";
+                        message.text = "Importando informacion de " + nombre + "...";
                         StartCoroutine(DownloadSpheres(nombre));
                     }
                     else
@@ -55,10 +55,11 @@ public class TouchController : MonoBehaviour
 
     public IEnumerator DownloadSpheres(string Description)
     {
-        Description = Description.Replace(" ", "%20");
-        Debug.Log(Description);
+
+        string descriptionSinEspacios = Description.Replace(" ", "%20");
+        
         // Pull down the JSON from our web-service
-        WWW w = new WWW("https://serverpmi.tr3sco.net/api/KPIs?Description=" + Description);
+        WWW w = new WWW("https://serverpmi.tr3sco.net/api/KPIs?Description=" + descriptionSinEspacios);
         yield return w;
         EliminarSphere();
         print("Waiting for sphere definitions\n");
@@ -68,11 +69,11 @@ public class TouchController : MonoBehaviour
         print("Received sphere definitions\n");
         print(w.text);
         // Extract the spheres from our JSON results
-        ExtractSpheres(w.text);
+        ExtractSpheres(Description, w.text);
 
     }
 
-    void ExtractSpheres(string json)
+    void ExtractSpheres(string description, string json)
     {
         string json2 = "{\"valores\":" + json + "}";
         var kpis = KPICollection.CreateFromJSON(json2).valores.OrderByDescending(h => h.YTD);
@@ -85,7 +86,7 @@ public class TouchController : MonoBehaviour
         }
 
 
-        message.text = "Conversion de datos a graficas..." ;
+        message.text = "Conversion de datos a graficas...";
         float x = 0, y = -0.5f, z = 3, r = 0.03f;
         x = -(0.06f * ((kpis.Count() / 2)));
         int columna = 0;
@@ -94,28 +95,30 @@ public class TouchController : MonoBehaviour
             columna++;
 
             GameObject sphere = GameObject.CreatePrimitive(PrimitiveType.Cube);
-			GameObject text = new GameObject();
+            GameObject text = new GameObject();
 
 
-			TextMesh t = text.AddComponent<TextMesh>();
-			t.text = item.Description;
-			t.fontSize = 30;
+            TextMesh t = text.AddComponent<TextMesh>();
+            t.text = item.YTD.ToString();
+            t.fontSize = 30;
 
             float float_YTD = 0.01f * Convert.ToSingle(item.porcentaje);
             sphere.name = float_YTD.ToString();
+            t.name = "text_" + item.YTD; 
+            
 
             sphere.transform.position = new Vector3(x, y + (float_YTD / 2), z);
-			t.transform.localPosition = new Vector3(x, (y + ((float_YTD / 2))+ float_YTD) + 0.02f, z);
+            t.transform.localPosition = new Vector3(x, (y + ((float_YTD / 2)) + float_YTD) + 0.002f, z);
 
 
             x = x + (2 * (r + 0.002f));
             float d = 2 * r;
 
 
-			t.transform.localEulerAngles += new Vector3(90, 0, 0);
-
+            t.transform.localEulerAngles = new Vector3(0, 0, 45);
 
             sphere.transform.localScale = new Vector3(d, d + float_YTD, d);
+            t.transform.localScale = new Vector3(0.02f,0.02f, 1);
 
             UnityEngine.Color col = UnityEngine.Color.white;
             switch (columna)
